7302c3b756cfbd395b7b6aca36aa26318989a0f3
Paweł Bogdanowicz <pawelb021@gmail.com>
2026-05-07 23:27:12 +0200

    added resursive field path and slice support

--- valid.go
+++ valid.go
@@ -1,29 +1,34 @@
 package valid
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
 	"strings"
 )
 
 const (
-	tagName      = "valid"
+	tagName      = "validate"
 	tagSeparator = ","
 )
 
-type ValidationError struct {
-	Field   string `json:"field"`
-	Rule    string `json:"rule"`
-	Value   any    `json:"value,omitempty"`
-	Message string `json:"message"`
+type ViolationError struct {
+	Path  string `json:"path"`
+	Rule  string `json:"rule"`
+	Value any    `json:"value,omitempty"`
+	Msg   string `json:"msg"`
 }
 
-func (e ValidationError) Error() string {
-	return fmt.Sprintf("%s: %s", e.Field, e.Message)
+// TODO: Temporary body
+func (e ViolationError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Path, e.Msg)
 }
 
-type ValidationErrors []ValidationError
+type Violation struct {
+	Value any
+	Msg   string
+}
+
+type ValidationErrors []ViolationError
 
 func (e ValidationErrors) Error() string {
 	errStrings := make([]string, 0, len(e))
@@ -35,7 +40,7 @@ func (e ValidationErrors) Error() string {
 }
 
 type Rule interface {
-	Validate(value any, field reflect.StructField) error
+	Validate(value any) (*Violation, error)
 	Msg(message string) Rule
 }
 
@@ -62,7 +67,7 @@ func (v *Validator) Check(value any) error {
 		return fmt.Errorf("expected struct, got %s", rt.Kind())
 	}
 
-	validationErrors, err := v.validateStruct(rt, rv)
+	validationErrors, err := v.validateStruct(rt, rv, "")
 	if err != nil {
 		return err
 	}
@@ -71,7 +76,7 @@ func (v *Validator) Check(value any) error {
 }
 
 // TODO: Think of a better name
-func (v *Validator) validateStruct(rt reflect.Type, rv reflect.Value) (ValidationErrors, error) {
+func (v *Validator) validateStruct(rt reflect.Type, rv reflect.Value, path string) (ValidationErrors, error) {
 	var validationErrors ValidationErrors
 
 	for i := 0; i < rt.NumField(); i++ {
@@ -83,38 +88,87 @@ func (v *Validator) validateStruct(rt reflect.Type, rv reflect.Value) (Validatio
 			return nil, fmt.Errorf("field %s has an empty tag", fieldType.Name)
 		}
 
-		for _, ruleName := range strings.Split(tag, tagSeparator) {
-			rule, ok := v.Rules[ruleName]
-			if !ok {
-				return nil, fmt.Errorf("rule %s doesn't exist", ruleName)
-			}
-
-			err := rule.Validate(fieldValue.Interface(), fieldType)
-			if err == nil {
-				continue
-			}
-
-			validationError, ok := errors.AsType[ValidationError](err)
-			if !ok {
-				return nil, err
-			}
-
-			validationErrors = append(validationErrors, validationError)
+		fieldValidationErrors, err := v.validateFieldRules(
+			fieldValue.Interface(),
+			formatPath(path, fieldType.Name),
+			strings.Split(tag, tagSeparator),
+		)
+		if err != nil {
+			return nil, err
 		}
 
-		ft := fieldType.Type
-		if ft.Kind() == reflect.Ptr {
-			ft = ft.Elem()
+		validationErrors = append(validationErrors, fieldValidationErrors...)
+
+		fv := fieldValue
+		if fv.Kind() == reflect.Ptr {
+			fv = fv.Elem()
 		}
 
-		if ft.Kind() == reflect.Struct {
-			validationErrs, err := v.validateStruct(ft, rv)
+		switch fv.Kind() {
+		case reflect.Struct:
+			validationErrs, err := v.validateStruct(fv.Type(), fv, formatPath(path, fieldType.Name))
 			if err != nil {
 				return nil, err
 			}
 
 			validationErrors = append(validationErrors, validationErrs...)
+
+		case reflect.Slice:
+			for j := 0; j < fv.Len(); j++ {
+				elemValue := fv.Index(j)
+
+				if elemValue.Kind() == reflect.Ptr {
+					if elemValue.IsNil() {
+						continue
+					}
+					elemValue = elemValue.Elem()
+				}
+
+				if elemValue.Kind() != reflect.Struct {
+					continue
+				}
+
+				validationErrs, err := v.validateStruct(
+					elemValue.Type(),
+					elemValue,
+					fmt.Sprintf("%s[%d]", formatPath(path, fieldType.Name), j),
+				)
+				if err != nil {
+					return nil, err
+				}
+
+				validationErrors = append(validationErrors, validationErrs...)
+			}
 		}
+	}
+
+	return validationErrors, nil
+}
+
+func (v *Validator) validateFieldRules(value any, path string, rules []string) (ValidationErrors, error) {
+	var validationErrors ValidationErrors
+
+	for _, ruleName := range rules {
+		rule, ok := v.Rules[ruleName]
+		if !ok {
+			return nil, fmt.Errorf("rule %s doesn't exist", ruleName)
+		}
+
+		violation, err := rule.Validate(value)
+		if err != nil {
+			return nil, err
+		}
+
+		if violation == nil {
+			continue
+		}
+
+		validationErrors = append(validationErrors, ViolationError{
+			Path:  path,
+			Rule:  ruleName,
+			Value: violation.Value,
+			Msg:   violation.Msg,
+		})
 	}
 
 	return validationErrors, nil
@@ -124,4 +178,12 @@ var validator = New()
 
 func Check(value any) error {
 	return validator.Check(value)
+}
+
+func formatPath(parent, child string) string {
+	if parent == "" {
+		return child
+	}
+
+	return fmt.Sprintf("%s.%s", parent, child)
 }
--- valid_test.go
+++ valid_test.go
@@ -3,19 +3,16 @@ package valid
 import (
 	"encoding/json"
 	"fmt"
-	"reflect"
 	"testing"
 )
 
 type testAAA struct{}
 
-func (t testAAA) Validate(value any, field reflect.StructField) error {
-	return ValidationError{
-		Field:   field.Name,
-		Rule:    "abc",
-		Value:   value,
-		Message: "aaa",
-	}
+func (t testAAA) Validate(value any) (*Violation, error) {
+	return &Violation{
+		Value: value,
+		Msg:   "aaa",
+	}, nil
 }
 
 func (t testAAA) Msg(message string) Rule {
@@ -24,12 +21,12 @@ func (t testAAA) Msg(message string) Rule {
 
 func TestCheck(t *testing.T) {
 	type test2Struct struct {
-		WWW string `valid:"abc"`
+		WWW string `validate:"abc"`
 	}
 	type testStruct struct {
-		Abc string      `valid:"abc"`
-		Cba string      `valid:"abc"`
-		AAA test2Struct `valid:"abc"`
+		Abc string        `validate:"abc"`
+		Cba string        `validate:"abc"`
+		AAA []test2Struct `validate:"abc"`
 	}
 
 	v := New()
@@ -38,8 +35,13 @@ func TestCheck(t *testing.T) {
 	err := v.Check(testStruct{
 		Abc: "123",
 		Cba: "456",
-		AAA: test2Struct{
-			WWW: "789",
+		AAA: []test2Struct{
+			{
+				WWW: "789",
+			},
+			{
+				WWW: "123",
+			},
 		},
 	})
 	if err != nil {
